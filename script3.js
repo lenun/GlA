b65ce56e25b1a6e50029a20190afdaca88a6d41f
Lena <rishes.lena@yandex.ru>
2020-06-12 19:47:19 +0700

    наконец-то!

---
+++ script3.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const isNumber = number => {
+    return (!isNaN(parseFloat(number)) && isFinite(number));
+};
+
+function game(){
+    let num=56;
+    let play;
+
+function start () {
+
+        let useNum = prompt("Сможешь отгадать загаданное число от 1 до 100?");
+    
+        if(useNum === null) {
+            let endPlay = confirm("Желаете закончить игру?");
+            if(endPlay) {
+                return 1;
+            }
+        }
+        
+        if(!isNumber(useNum)) {
+            alert("Введи число!");
+            return start();
+        }
+    
+        if(useNum < num) {
+            alert("Загаданное число больше");
+            return start();
+        }
+    
+        if(useNum > num) {
+            alert("Загаданное число меньше");
+            return start();
+        }
+    
+        play = confirm('Вы выйграли');
+    
+      }
+      start();
+    }
+game();
