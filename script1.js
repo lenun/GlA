12d8a80e5cf8d9b80bc6fd95d45e88033026555a
lenun <66440344+lenun@users.noreply.github.com>
2020-06-15 06:23:38 +0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,3 +1,4 @@
+
 'use strict';
 let arr = ['2', '4', '100', '42', '32', '22', '299']; 
 
@@ -6,4 +7,18 @@ arr.forEach((rer) => {
     console.log(rer);
   }
 });
+const simpleNumber =function(number){
+    for (let i = 2; i < number; i++) {
+        if(number % i === 0) {
+            return 0;
+        }
 
+    }return number > 1;
+};
+for (let i = 1; i <= 100; i++) {
+    if(!simpleNumber(i)) {
+        continue;
+    } else {
+        console.log(i,'Делители : 1 и', i);
+    }
+}
