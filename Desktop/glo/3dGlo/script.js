8f66929f5eeedf2a3d172820dada6e2335b83156
Lena <rishes.lena@yandex.ru>
2020-07-03 04:12:50 +0700

    popup

--- Desktop/glo/3dGlo/script.js
+++ Desktop/glo/3dGlo/script.js
@@ -5,7 +5,8 @@ window.addEventListener('DOMContentLoaded', () => {
         let timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
-
+ 
+    //таймер
     function getTimeRemaing(deadline){
         let dataStop = new Date(deadline).getTime(),
             dataNow = new Date().getTime(),
@@ -31,4 +32,69 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     }
     setInterval(updateClock, 1000, '03 jul 2020');
+
+const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+        menu = document.querySelector(' menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItem = document.querySelectorAll('ul>li');
+        
+    
+    let handLerMenu = () => {
+        if(!menu.style.transform || menu.style.transform === `translate(-100%)`){
+            menu.style.transform = `translate(0)`;
+        }else{
+            menu.style.transform = `translate(-100%)`;
+        }
+    };
+
+    btnMenu.addEventListener('click', handLerMenu);
+    closeBtn.addEventListener('click',handLerMenu);
+    menuItem.forEach((elem) => elem.addEventListener('click',handLerMenu));
+};
+    //попит
+  const togglePopup = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn');
+      console.log(popup);
+    let 
+      count = 0,
+      gap;
+
+    const animation = () => {
+      gap = requestAnimationFrame(animation);
+      count++;
+
+      if (count <= 50) {
+        popup.style.opacity = count / 50;
+      } else {
+        count = 0;
+        cancelAnimationFrame(gap);
+      }
+    };
+
+    popupBtn.forEach((elem) =>
+      elem.addEventListener('click', () => {
+        if (document.documentElement.clientWidth >= 768) {
+          gap = requestAnimationFrame(animation);
+        }
+        popup.style.display = 'block';
+      })
+    );
+
+    popup.addEventListener('click', (event) => {
+      let target = event.target;
+
+      if (target.classList.contains('popup-close')) {
+        popup.style.display = 'none';
+      } else {
+        target = target.closest('.popup-content');
+        if (!target) {
+          popup.style.display = 'none';
+        }
+      }
+    });
+  };
+  toggleMenu();
+  togglePopup();
 });
