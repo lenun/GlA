f9c169001844aab65885e8e4bf2294980b9258a6
Lena <rishes.lena@yandex.ru>
2020-07-08 22:45:09 +0700

    3DGLO23

--- Desktop/glo/3dGlo/script.js
+++ Desktop/glo/3dGlo/script.js
@@ -236,7 +236,43 @@ const getstartSlider = () => {
             startSlide();
           }
         });
-   
+  
+//наша команда
+
+const getCommand = () => {
+  const elementCommand = document.querySelector('.command>.container>.row');
+    console.log(elementCommand);
+  
+  const changeImg = (event) => {
+    const target = event.target;
+
+    if(target.classList.contains('command__photo')){
+      let src = target.src;
+      target.src = target.dataset.img;
+      target.dataset.img = src ;
+    }
+  };
+  elementCommand.addEventListener('mouseenter',changeImg);
+  elementCommand.addEventListener('mouseout',changeImg);
+
 };
+getCommand();
+
+//калькулятор 
+
+const calculator = () =>{
+    const calcBlock = document.querySelector(".calc-block");
+
+    const input = (event) => {
+        const target = event.target;
+
+        if(target.tagName === 'INPUT') {
+            target.value = target.value.replace(/\D/, '');
+        }
+    };
+    calcBlock.addEventListener('input', input);
+  };
+calculator();   
+}
   getstartSlider(3000);
 });
